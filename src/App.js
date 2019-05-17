61a07dc128eac93ea55204ed936c25daa084160a
jianghonglin608 <JIANGHONGLIN608@pingan.com.cn>
2019-05-17 17:45:58 +0800

    second

--- src/App.js
+++ src/App.js
@@ -5,21 +5,26 @@ import { TransitionGroup, CSSTransition } from 'react-transition-group'
 import Load from './views/load';
 import start from './views/start';
 import './statics/css/reset.scss';
-import '../statics/css/animate'
+import './statics/css/animate.scss'
 
 class App extends Component {
   // constructor(...args) {
   //   super(...args);
   // }
   render() {
+    const location = this.props.location || {key:''}
     return (
       <BrowserRouter>
         <div>
+          <TransitionGroup>
+            <CSSTransition key={location.key}  timeout={1000} classNames="fade">
             <Switch>
               <Route exact path='/' component={Load}></Route>
               <Route path='/load' component={Load}></Route>
               <Route path='/start' component={start}></Route>   
             </Switch>
+            </CSSTransition>
+          </TransitionGroup>
         </div>
       </BrowserRouter>
     )
